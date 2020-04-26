b8538361c57b0ad2c9d98d38e7764680bd4a6b37
faizan uddin <faizan.uddin121@gmail.com>
2020-04-26 17:00:16 -0500

    added react and inc security exp_time

---
+++ src/main/resources/static/precache-manifest.36402a3660d70e4efbc0ac36976cca52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e753cd130a3ba776256c182ac05db8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "016a545c1b0bb3a339a7",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "303e5e7529bee05c59d8",
+    "url": "/static/css/main.f761ed1a.chunk.css"
+  },
+  {
+    "revision": "016a545c1b0bb3a339a7",
+    "url": "/static/js/2.ac2dfa20.chunk.js"
+  },
+  {
+    "revision": "303e5e7529bee05c59d8",
+    "url": "/static/js/main.5417ca92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
